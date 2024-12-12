8fe16238d5a78ace8a14ac4ef5da1b97c8805369
Miguel Ramos <return.infox@gmail.com>
2024-12-12 15:17:54 -0300

    terminamos con los settings

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import GlobalContextProvider from './src/context/GlobalContext'
+import { PaperProvider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import BottomTabsNavigator from './src/navigation/BottomTabsNavigator'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GlobalContextProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <BottomTabsNavigator />
+        </NavigationContainer>
+      </PaperProvider>
+    </GlobalContextProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
+import { List, Switch } from 'react-native-paper';
 
-const SettingsScreen = () => {
+const SettingsScreen: React.FC = () => {
+
+  const { showTraffic, setShowTraffic  } = useContext(GlobalContext);
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={styles.container}>
+      <List.Section>
+        <List.Subheader>Configuraciones Generales</List.Subheader>
+        <List.Item
+          title="Mostrar tráffico en tiempo real"
+          description= "Habilita o deshabilita la visualizacion del trafico"
+          right={() => (
+            <Switch
+              value={showTraffic}
+              onValueChange={() => setShowTraffic(!showTraffic)}
+            />
+          )}
+        >
+
+        </List.Item>
+      </List.Section>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  }
+});
 
 export default SettingsScreen
